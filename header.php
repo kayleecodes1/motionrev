3822f1b28435aef3f39c05d5ad54c963c44d0333
Kyle Maguire <kyle@prod4ever.com>
2013-10-29 00:27:48 -0400

    Add favicons for all devices.

--- header.php
+++ header.php
@@ -3,6 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Motion Revolution</title>
+
+	<link rel="shortcut icon" type="image/x-icon" href="library/img/favicon.ico">
+	<link rel="apple-touch-icon" sizes="114×114" href="library/img/touch-icon-114×114.png" />
+	<link rel="apple-touch-icon" sizes="72×72" href="library/img/touch-icon-72×72.png" />
+	<link rel="apple-touch-icon" href="library/img/touch-icon-iphone.png" />
+
 	<link type="text/css" rel="stylesheet" href="/library/css/bootstrap.min.css" />
 	<link type="text/css" rel="stylesheet" href="/library/css/main.css" />
 </head>
